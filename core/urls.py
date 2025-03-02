2fd4f6a967c681fbe793d95a4d1f08ea968924b6
stEElpuLse <100028301+stEElpuLs3@users.noreply.github.com>
2025-03-02 20:05:56 -0300

    new rota

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('projetos/', views.projetos, name='projetos'),
     path('curriculo/', views.curriculo, name='curriculo'),
 ]
+
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
